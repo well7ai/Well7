e28f6380020e113a9499135bf78366c8f9d37086
ahmedalmusawa <155393209+ahmedalmusawa@users.noreply.github.com>
2026-04-08 23:31:02 +0100

    dark

--- src/App.jsx
+++ src/App.jsx
@@ -599,10 +599,10 @@ export default function App() {
                        <div className="h-64 border border-lightest-gray rounded-[16px] p-4 bg-snow">
                          <ResponsiveContainer width="100%" height="100%">
                             <RadarChart cx="50%" cy="50%" outerRadius="70%" data={radarData}>
-                              <PolarGrid stroke="#d9d9dd" />
+                              <PolarGrid stroke="#333333" />
                               <PolarAngleAxis dataKey="metric" tick={{fill: '#93939f', fontSize: 12, fontFamily: 'JetBrains Mono'}} />
                               <PolarRadiusAxis angle={30} domain={[0, 100]} tick={false} axisLine={false}/>
-                              <Radar name="Score" dataKey="A" stroke="#000000" fill="#000000" fillOpacity={0.1} />
+                              <Radar name="Score" dataKey="A" stroke="#ffffff" fill="#ffffff" fillOpacity={0.1} />
                             </RadarChart>
                           </ResponsiveContainer>
                        </div>
